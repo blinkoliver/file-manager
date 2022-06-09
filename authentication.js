c40ed678df280937b62657ec777e45b574494382
Anton Barouka <borovko28@gmail.com>
2022-06-10 00:44:17 +0300

    feat: main listener and list

--- authentication.js
+++ authentication.js
@@ -1,9 +1,11 @@
 import { argv } from "process";
+import { homedir } from "os";
 
 const authentication = () => {
   const userInputArgv = argv.slice(3);
   const user = userInputArgv[0].replace("--username=", "");
   console.log(`Welcome to the File Manager, ${user}!`);
+  console.log(`You are currently in ${homedir}`);
   const exitHandler = (options, exitCode) => {
     if (options.exit || options.cleanup) {
       console.log(`\nThank you for using File Manager, ${user}!`);
--- index.js
+++ index.js
@@ -3,6 +3,11 @@ import authentication from "./authentication.js";
 
 const start = async () => {
   stdin.resume();
+  const myCallback = (userInput) => {
+    const input = userInput.toString().trim();
+    console.log(input);
+  };
+  stdin.on("data", myCallback);
   authentication();
 };
 
